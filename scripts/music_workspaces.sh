f581376f27adb0460974b91ffb4ebdd10e2d312e
Nenosaj <jasondaohog1@gmail.com>
2025-08-01 23:17:36 +0800

    music configuration

--- scripts/music_workspaces.sh
+++ scripts/music_workspaces.sh
@@ -7,6 +7,11 @@ ws="10"
 sleep 0.2
 
 i3-msg "workspace $ws; exec kitty --config ~/.config/kitty/ncmpterm.conf --class ncmpterm --title ncmp-browser -e ncmpcpp --screen=browser"
+
+sleep 0.2
+
+i3-msg "workspace $ws; exec kitty --directory ~/dotfiles/scripts/ --config ~/.config/kitty/ncmpterm.conf --class npcterm --title yt-search"
+
 sleep 0.2
 
 i3-msg "workspace $ws; exec kitty --config ~/.config/kitty/ncmpterm.conf --class ncmpterm --title ncmp-visualizer -e ncmpcpp --screen=visualizer"
