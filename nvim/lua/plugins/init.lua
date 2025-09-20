02827d34fdcc9a7204225f503acf728c3a638c4b
Nenosaj <jasondaohog1@gmail.com>
2025-09-20 20:19:23 +0800

    added init-lua for /plugins, added latex for completions

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,10 +1,8 @@
 return {
     require("plugins.alpha"),
     require("plugins.completions"),
-    require("plugins.gruvbox"),
     require("plugins.lualine"),
     require("plugins.neorg"),
-    require("plugins.none-ls"),
     require("plugins.oil"),
     require("plugins.telescope"),
     require("plugins.treesitter"),
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,16 +1,14 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  name = "telescope",
-  tag = "0.1.8",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  config = function()
-    local telescope = require("telescope")
-    local builtin = require("telescope.builtin")
+	"nvim-telescope/telescope.nvim",
+	name = "telescope",
+	tag = "0.1.8",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local telescope = require("telescope")
+		local builtin = require("telescope.builtin")
 
-      -- Keymaps
-    vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-    vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-    vim.keymap.set("n", "<leader>/", "<cmd>Telescope current_buffer_fuzzy_find<cr>", {})
-  
-  end,
+		vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+		vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+		vim.keymap.set("n", "<leader>/", "<cmd>Telescope current_buffer_fuzzy_find<cr>", {})
+	end,
 }
